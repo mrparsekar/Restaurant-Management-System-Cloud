42ae1cb77b159ec1e46669cc2806d9481207beaa
Shreyash Parsekar <101310326+mrparsekar@users.noreply.github.com>
2025-10-25 12:30:27 +0530

    Update menu fetch URL to use backend service

--- restaurant-frontend/src/components/Menu.js
+++ restaurant-frontend/src/components/Menu.js
@@ -13,7 +13,7 @@ const Menu = ({ setCartCount }) => {
 
   useEffect(() => {
     // Fetch menu items
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/menu`)
+    fetch("https://restaurant-cloud-backend.azurewebsites.net/menu")
       .then((res) => res.json())
       .then((data) => {
         console.log("Menu data fetched:", data);
